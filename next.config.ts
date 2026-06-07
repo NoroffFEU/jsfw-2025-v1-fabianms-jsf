7f670df8f7e33ef37bb21353c5fadc65f7e9c4ce
Fabian Sandberg <fabsan03935@stud.noroff.no>
2026-06-07 02:21:00 +0200

    6

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Make sure output: 'export' is NOT here
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
